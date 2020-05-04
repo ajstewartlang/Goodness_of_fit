3f0974458d33341269b69a5bad36a0f4492e237f
ajstewartlang <andrew.stewart@manchester.ac.uk>
2020-05-04 16:09:04 +0100

    tidied up files and folders

--- analysis_script.R
+++ analysis_script.R
@@ -5,15 +5,13 @@ library(lmerTest)
 library(emmeans)
 library(performance)
 
-##########################
-## EXPLANATORY GOODNESS ##
-##########################
+# Explanatory Goodness Start####
 
-data <- read_csv("Explanation_QUALITY.csv")
-data <- data[3:nrow(data),]
-nrow(data) # 157
+my_data <- read_csv("questionnaire_data/Explanation_QUALITY.csv")
+my_data <- my_data[3:nrow(my_data),]
+nrow(damy_datata) # 157
 
-dat0 <- filter(data, Finished == 1)
+dat0 <- filter(my_data, Finished == 1)
 nrow(dat0) # 142
 dat <- filter(dat0, check == "7" & serious == "1" & UserLanguage == "EN")
 nrow(dat) # 134
@@ -36,45 +34,8 @@ weak <- res[c(seq(2, 18, 2), seq(19, 35, 2))]
 
 results <- c(strong, weak)
 
-###########
-## TRUTH ##
-###########
-
-dataT <- read_csv("Explanation_TRUTH.csv")
-dataT <- dataT[3:nrow(dataT),]
-nrow(dataT) # 158
-
-dat0T <- filter(dataT, Finished == 1)
-nrow(dat0T) # 144
-datT <- filter(dat0T, check == "7" & serious == "1" & UserLanguage == "EN")
-nrow(datT) # 138
-
-# time (in secs) spent on survey
-describe(as.numeric(as.data.frame(datT)[,6]))
-
-# gender
-table(datT$Gender)
-
-# age
-describe(as.numeric(datT$Age))
-
-# mean responses
-resT <- sapply(23:58, function(x) mean(as.numeric(as.data.frame(datT)[,x]), na.rm = T))
-
-# in the survey, we interchanged strong and weak explanations in the two groups
-strongT <- resT[c(seq(1, 17, 2), seq(20, 36, 2))]
-weakT <- resT[c(seq(2, 18, 2), seq(19, 35, 2))]
-
-resultsT <- c(strongT, weakT)
-
-#################
-## CORRELATION ##
-#################
-
-cor.test(results, resultsT)
-
 # Andrew's code below
-# Explanatory goodness ####
+# Explanatory Goodness Main####
 
 summary <- dat %>% 
   pivot_longer(cols = colnames(dat)[23:58], names_to = "Item", values_to = "Score") %>%
@@ -162,7 +123,37 @@ model_spillover <- lmer(spillover ~ mean_score + (1 | P.s) + (1 | Item), data = 
 summary(model_spillover)
 check_model(model_spillover)
 
-#Truth ####
+# Truth Start ####
+
+dataT <- read_csv("questionnaire_data/Explanation_TRUTH.csv")
+dataT <- dataT[3:nrow(dataT),]
+nrow(dataT) # 158
+
+dat0T <- filter(dataT, Finished == 1)
+nrow(dat0T) # 144
+datT <- filter(dat0T, check == "7" & serious == "1" & UserLanguage == "EN")
+nrow(datT) # 138
+
+# time (in secs) spent on survey
+describe(as.numeric(as.data.frame(datT)[,6]))
+
+# gender
+table(datT$Gender)
+
+# age
+describe(as.numeric(datT$Age))
+
+# mean responses
+resT <- sapply(23:58, function(x) mean(as.numeric(as.data.frame(datT)[,x]), na.rm = T))
+
+# in the survey, we interchanged strong and weak explanations in the two groups
+strongT <- resT[c(seq(1, 17, 2), seq(20, 36, 2))]
+weakT <- resT[c(seq(2, 18, 2), seq(19, 35, 2))]
+
+resultsT <- c(strongT, weakT)
+
+#Truth Main ####
+
 summary <- datT %>% 
   pivot_longer(cols = colnames(datT)[23:58], names_to = "Item", values_to = "Score") %>%
   select(Item, Score) %>%
@@ -241,7 +232,7 @@ model_antecedent <- lmer(Antecedent ~ mean_score + (1 | P.s) + (1 | Item), data 
 summary(model_antecedent)
 check_model(model_antecedent)
 
-model_consequent<- lmer(Consequent ~ mean_score + (1 | P.s) , data = joined_data_tt)
+model_consequent<- lmer(Consequent ~ mean_score + (1 | P.s) + (1 | Item), data = joined_data_tt)
 summary(model_consequent)
 check_model(model_consequent)
 
